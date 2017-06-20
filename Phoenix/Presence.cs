d069be7b9647ce50eb0717f5a58be21b3bc58c09
Jonathan Leang <jonathanleang@gmail.com>
2017-06-20 10:10:14 +0800

    Implement Presence #1
    - no test cases
    - don’t merge

--- Phoenix/Presence.cs
+++ Phoenix/Presence.cs
@@ -1,3 +1,9 @@
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
 namespace Phoenix {
 	// ## Presence
 	//
@@ -40,6 +46,138 @@ namespace Phoenix {
 	//     let onlineUsers = Presence.list(state, listBy)
 	//
 	public class Presence {
-		// TODO
+		public static JObject syncState(JObject currentState, JObject newState, Action<string, JToken, JToken> onJoin = null, Action<string, JToken, JToken> onLeave = null){
+//			JObject state = (JObject)currentState.DeepClone();
+			JObject state = (JObject)JObject.Parse(currentState.ToString());
+			JObject joins = new JObject();
+			JObject leaves = new JObject();
+
+			IList<string> keys = state.Properties().Select(p => p.Name).ToList();
+			foreach (string key in keys) {
+				if(newState[key] == null){
+					leaves[key] = state[key];
+				}
+			}
+
+			IList<string> newkeys = newState.Properties().Select(p => p.Name).ToList();	
+			foreach (string key in newkeys) {
+				var newPresence = newState [key];
+				var currentPresence = state[key];
+				if(currentPresence != null){
+					List<JToken> newRefs = getPhxRef ((JArray) newPresence ["metas"]);
+					List<JToken> curRefs = getPhxRef ((JArray) currentPresence ["metas"]);
+
+					List<JToken> joinedMetas = newPresence["metas"]
+						.Where (newPresenceMeta => curRefs.Exists (curRef => curRef == newPresenceMeta ["phx_ref"]))
+						.ToList();
+					
+					List<JToken> leftMetas = currentPresence["metas"]
+						.Where (currentPresenceMeta => newRefs.Exists (newRef => newRef == currentPresenceMeta ["phx_ref"]))
+						.ToList();
+
+					if(joinedMetas.Count != 0){
+						joins[key] = newPresence;
+						joins[key]["metas"] = new JArray (
+							joinedMetas.Select (p => new JObject {
+								{ "phx_ref", p["phx_ref"]},
+								{ "online_at", p["online_at"]}
+							})
+						);
+					}
+					if(leftMetas.Count != 0){
+						leaves[key] = currentPresence.DeepClone();
+						leaves[key]["metas"] = new JArray (
+							leftMetas.Select (p => new JObject {
+								{ "phx_ref", p["phx_ref"]},
+								{ "online_at", p["online_at"]}
+							})
+						);
+					}
+				} else {
+					joins[key] = newPresence;
+				}
+			}
+			JObject diffState = new JObject ();
+			diffState ["joins"] = joins;
+			diffState ["leaves"] = leaves;
+			return syncDiff( state, diffState, onJoin, onLeave);
+		}
+
+		public static JObject syncDiff(JObject currentState, JObject diffState, Action<string, JToken, JToken> onJoin = null, Action<string, JToken, JToken> onLeave = null){
+			JObject joins = (JObject)diffState ["joins"];
+			JObject leaves = (JObject)diffState ["leaves"];
+			JObject state = (JObject)currentState.DeepClone();
+
+//			this.map(joins, (key, newPresence) => {
+//				let currentPresence = state[key]
+//				state[key] = newPresence
+//				if(currentPresence){
+//					state[key].metas.unshift(...currentPresence.metas)
+//				}
+//				onJoin(key, currentPresence, newPresence)
+//			})
+
+			IList<string> joinskeys = joins.Properties().Select(p => p.Name).ToList();	
+			foreach (string key in joinskeys) {
+				var newPresence = joins[key];
+				var currentPresence = state [key];
+				state [key] = newPresence;
+				if (currentPresence != null) {
+//					Debug.Log (state [key]["metas"]); // WTF
+					((JArray)state [key]["metas"]).Merge(currentPresence["metas"]);
+//					Debug.Log (state [key]["metas"]);
+				}
+				if (onJoin != null) { 
+					onJoin (key, currentPresence, newPresence);
+				}
+			}
+
+			IList<string> leaveskeys = leaves.Properties().Select(p => p.Name).ToList();	
+			foreach (string key in leaveskeys) {
+				var leftPresence = leaves[key];
+				var currentPresence = state [key];
+				if(currentPresence != null){
+					List<JToken> refsToRemove = getPhxRef ((JArray) leftPresence ["metas"]);
+//					Debug.Log (currentPresence ["metas"]);
+					List<JToken> currentPresenceMeta = currentPresence ["metas"]
+						.Where (currectMeta => refsToRemove.Exists (newRef => newRef == currectMeta ["phx_ref"]))
+						.ToList();
+
+					currentPresence ["metas"] = new JArray (
+						currentPresenceMeta.Select (p => new JObject {
+							{ "phx_ref", p["phx_ref"]},
+							{ "online_at", p["online_at"]}
+						})
+					);
+
+					if(onLeave != null){
+						onLeave(key, currentPresence, leftPresence);
+					}
+					if(currentPresence["metas"].Count() == 0){
+						state.Remove(key);
+					}
+				}
+			}
+			return state;
+		}
+
+		public static void list(JObject presences,  Action<string, JToken> chooser){
+			if(chooser == null){ 
+				return;
+			}
+			IList<string> keys = presences.Properties().Select(p => p.Name).ToList();	
+			foreach (string key in keys) {
+				chooser (key, presences[key]);
+			}
+		}
+
+		static List<JToken> getPhxRef(JArray metas){
+			List<JToken> newRefs = new List<JToken>();
+			foreach (JObject newPresenceMeta in metas ) {
+				newRefs.Add (newPresenceMeta["phx_ref"]);
+			}
+			return newRefs;
+		}
+
 	}
 }
