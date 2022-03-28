310d6c5eb78a5677aa11ae0fb83e9bf1f7e6eaa1
Maz Jaleel <mazjaleel@gmail.com>
2022-03-28 21:44:41 +0300

    [WIP]: Presence

--- Phoenix/Presence.cs
+++ Phoenix/Presence.cs
@@ -41,6 +41,11 @@ namespace Phoenix {
 
 		#region nested types
 
+		public sealed class Options {
+			public string stateEvent = "presence_state";
+			public string diffEvent = "presence_diff";
+		}
+
 		public sealed class Metadata {
 			public uint id;
 			public string state;
@@ -73,30 +78,45 @@ namespace Phoenix {
 
 		#endregion
 
+		public State state = new();
+		private List<Diff> pendingDiffs = new();
 		private Channel channel;
-		private readonly State state = new();
 		private string joinRef = null;
 
-		public Presence(Channel channel) {
+		public Presence(Channel channel, Options opts = null) {
 			this.channel = channel;
 
-			// TODO: Add Presence Event enum under InboundEvent
-			channel.On("events.state", newState => {
+			var options = opts ?? new();
+
+			channel.On(options.stateEvent, newState => {
 				joinRef = channel.joinRef;
+				state = SyncState(state, newState, OnJoin, OnLeave);
+
+				state = pendingDiffs.Aggregate(
+					state, 
+					(state, diff) => SyncDiff(state, diff, OnJoin, OnLeave)
+				);
+
+				pendingDiffs.Clear();
 
 				OnSync?.Invoke();
 			});
 
-			channel.On("events.diff", diff => {
-
+			channel.On(options.diffEvent, diff => {
+				if (InPendingSyncState()) {
+					pendingDiffs.Add(diff);
+				} else {
+					state = SyncDiff(state, diff, OnJoin, OnLeave);
+					OnSync?.Invoke();
+				}
 			});
 		}
 
 		public List<MetadataContainer> List(
 			Func<KeyValuePair<string, MetadataContainer>, MetadataContainer> by
-		) {
-			return List(state, by);
-		}
+		) => List(state, by);
+
+		public bool InPendingSyncState() => joinRef == null || joinRef != channel.joinRef;
 
 		// lower-level public static API
 
@@ -105,40 +125,6 @@ namespace Phoenix {
 		* with the client's state. An optional `onJoin` and `onLeave` callback can
 		* be provided to react to changes in the client's local presences across
 		* disconnects and reconnects with the server.
-		*
-		* @returns {Presence}
-		*
-		static syncState(currentState, newState, onJoin, onLeave){
-			let state = this.clone(currentState)
-			let joins = {}
-			let leaves = {}
-
-			this.map(state, (key, presence) => {
-				if(!newState[key]){
-					leaves[key] = presence
-				}
-			})
-			this.map(newState, (key, newPresence) => {
-				let currentPresence = state[key]
-				if(currentPresence){
-					let newRefs = newPresence.metas.map(m => m.phx_ref)
-					let curRefs = currentPresence.metas.map(m => m.phx_ref)
-					let joinedMetas = newPresence.metas.filter(m => curRefs.indexOf(m.phx_ref) < 0)
-					let leftMetas = currentPresence.metas.filter(m => newRefs.indexOf(m.phx_ref) < 0)
-					if(joinedMetas.length > 0){
-						joins[key] = newPresence
-						joins[key].metas = joinedMetas
-					}
-					if(leftMetas.length > 0){
-						leaves[key] = this.clone(currentPresence)
-						leaves[key].metas = leftMetas
-					}
-				} else {
-					joins[key] = newPresence
-				}
-			})
-			return this.syncDiff(state, {joins: joins, leaves: leaves}, onJoin, onLeave)
-		}
 		*/
 		public static State SyncState(
 			State currentState,
@@ -183,43 +169,10 @@ namespace Phoenix {
 		}
 
 		/**
-		*
 		* Used to sync a diff of presence join and leave
 		* events from the server, as they happen. Like `syncState`, `syncDiff`
 		* accepts optional `onJoin` and `onLeave` callbacks to react to a user
 		* joining or leaving from a device.
-		*
-		* @returns {Presence}
-		*
-		static syncDiff(state, diff, onJoin, onLeave){
-			let {joins, leaves} = this.clone(diff)
-			if(!onJoin){ onJoin = function (){ } }
-			if(!onLeave){ onLeave = function (){ } }
-
-			this.map(joins, (key, newPresence) => {
-				let currentPresence = state[key]
-				state[key] = this.clone(newPresence)
-				if(currentPresence){
-					let joinedRefs = state[key].metas.map(m => m.phx_ref)
-					let curMetas = currentPresence.metas.filter(m => joinedRefs.indexOf(m.phx_ref) < 0)
-					state[key].metas.unshift(...curMetas)
-				}
-				onJoin(key, currentPresence, newPresence)
-			})
-			this.map(leaves, (key, leftPresence) => {
-				let currentPresence = state[key]
-				if(!currentPresence){ return }
-				let refsToRemove = leftPresence.metas.map(m => m.phx_ref)
-				currentPresence.metas = currentPresence.metas.filter(p => {
-					return refsToRemove.indexOf(p.phx_ref) < 0
-				})
-				onLeave(key, currentPresence, leftPresence)
-				if(currentPresence.metas.length === 0){
-					delete state[key]
-				}
-			})
-			return state
-		}
 		*/
 		public static State SyncDiff(
 			State state, 
@@ -265,19 +218,6 @@ namespace Phoenix {
 
 		/**
 		* Returns the array of presences, with selected metadata.
-		*
-		* @param {Object} presences
-		* @param {Function} chooser
-		*
-		* @returns {Presence}
-		*
-		static list(presences, chooser){
-			if(!chooser){ chooser = function (key, pres){ return pres } }
-
-			return this.map(presences, (key, presence) => {
-				return chooser(key, presence)
-			})
-		}
 		*/
 		public static List<MetadataContainer> List(
 			State presences,
@@ -289,15 +229,5 @@ namespace Phoenix {
 
 			return presences.ToList().Select(chooser).ToList();
 		}
-
-		// private
-
-		/**
-		static map(obj, func){
-			return Object.getOwnPropertyNames(obj).map(key => func(key, obj[key]))
-		}
-
-		static clone(obj){ return JSON.parse(JSON.stringify(obj)) }
-		*/
 	}
 }
